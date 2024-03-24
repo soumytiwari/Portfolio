ec95e5ae769bcfaa3df6f52a1ab5ae86512bae39
soumytiwari <tiwarisoumya111@gmail.com>
2024-03-24 15:01:41 +0530

    few more animations done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+// import Tilt from 'react-tilt'            // to tilt the card
+// import { motion } from 'framer-motion';
+
+// import { styles } from '../styles';
+// import { services } from '../constants';
+// import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
 import { motion } from "framer-motion";
-
+import { useEffect, useState } from "react";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  // State to store animation offset
+  const [offsetY, setOffsetY] = useState(0);
+
+  // function to handle animation
+  useEffect(() => {
+    const startTime = performance.now(); // Get start time
+    const animateText = () => {
+      const elapsed = performance.now() - startTime; // Calculate elapsed time
+      const offsetY = Math.sin(elapsed / 350) * 10; // Calculate offset
+      setOffsetY(offsetY); // Update state with new offset
+      requestAnimationFrame(animateText);
+    };
+    requestAnimationFrame(animateText); // Start animation loop
+  }, []);       //  run only once on component mount
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
@@ -15,11 +30,14 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <motion.div
+            className={`${styles.heroHeadText} text-white`}
+            style={{ translateY: offsetY }} // Apply translateY animation
+          >
             Hi, I'm <span className="text-[#915eff]">Soumya</span>
-          </h1>
+          </motion.div>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D models, 3D and 2D animations,{" "}
+            I develop 3D models, 3D and 2D animations,
             <br className="sm:block hidden" />
             user interfaces and web applications.
           </p>
@@ -27,20 +45,25 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center" style={{ top: '820px' }}>
+      <div
+        className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center"
+        style={{ top: "820px" }}
+      >
         <a href="#about">
-          <div className="w-[35px h-[64px]
-          rounded-3xl border-4 border-secondary flex justify-center items-start p-2" >
-            <motion.dev 
-            animate={{
-              y:[0, 24, 0]
-            }}
-            transition={{
-              duration: 1.5,
-              repeat:Infinity,
-              repeatType: 'loop'
-            }}
-            className="w-3 h-3 rounded-full bg-secondary mb-1"
+          <div
+            className="w-[35px h-[64px]
+          rounded-3xl border-4 border-secondary flex justify-center items-start p-2"
+          >
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
