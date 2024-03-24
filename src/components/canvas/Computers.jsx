94122301919ba9882467185631851d495ba7d98e
soumytiwari <tiwarisoumya111@gmail.com>
2024-03-24 12:34:53 +0530

    entire hero section and computer-canvas is done including for mobile application

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,51 +1,79 @@
-import { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber"; //   this will give a background frame to work on for canvas
-import { OrbitControls, Preload, meshBounds, useGLTF } from "@react-three/drei"; //   few 3D model handling objects, 'useGLTF' is to get/render 3D model
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";          //   this will give a background frame to work on for canvas
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";          //   few 3D model handling objects, 'useGLTF' is to get/render 3D model
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <pointLight intensity={1} />
-      <spotLight position={[-20, 50, 10]}
-      angle={0.12}
-      penumbra={1}
-      intensity={1}
-      castShadow
-      shadow-mapSize={1024} />
-      <primitive 
-      object={computer.scene} 
-      scale={0.75}
-      position={[0, -3.25, -1.5]}
-      rotation={[-0.01, -0.2, -0.1]}
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -4, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+    // it is changing the 'isMobile' variable
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
+      dpr={[1, 2]}
       // x,y,z position of camera and feild of view of the model
       camera={{ position: [20, 3, 5], fov: 25 }}
       // this needs to be there to properly render our model
       gl={{ preserveDrawingBuffer: true }}
     >
+
       {/* not related to 3js, but to react. this allows us to have a loader while our model is loading */}
-      <Suspense fallback={<CanvasLoader/>}>
-        <OrbitControls 
-        enableZoom={false}        //  this will dis-allow us to let us zoom the model
-        // this will restrict the rotation, we will not be able to rotate it all the way around, up and down left and right but a specific axis
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2} 
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}        //  this will dis-allow us to let us zoom the model
+          // this will restrict the rotation, we will not be able to rotate it all the way around, up and down left and right but a specific axis
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
